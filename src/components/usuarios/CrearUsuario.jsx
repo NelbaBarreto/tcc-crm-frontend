9a614868b0fdae08aeb39435d9f7c5876d7794b7
Nelba Barreto <barretonelba@gmail.com>
2022-08-15 00:21:11 -0400

    Guardar datos de la persona en el state

--- src/components/usuarios/CrearUsuario.jsx
+++ src/components/usuarios/CrearUsuario.jsx
@@ -4,72 +4,88 @@ import ModalUsuario from "../empleados/ModalUsuario";
 import { useQuery } from "react-query";
 import { createUsuarios } from "../../api/usuarios";
 
-const CrearUsuario = () => {
-  const [persona, setPersona] = useState({ empleado: {}, usuario: {} });
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-
-  const crear = async e => {
-    e.preventDefault();
-    await createUsuarios(persona);
-  };
-
-  const handleChange = ({ campo, valor }) => {
-    console.log(campo, valor);
-  };
-
+const DatosPersona = ({ persona, setPersona }) => {
   const options = [
     { value: "ci", label: "CI" },
     { value: "ruc", label: "RUC" },
     { value: "pasaporte", label: "Pasaporte" }
   ]
 
-  const DatosPersona = () => {
-    return (
-      <section>
-        <div className="divider">Datos Personales</div>
-        <div className="field">
-          <label className="label">Nombre</label>
-          <div className="control">
-            <input
-              className="input shadow-lg"
-              type="text"
-            />
-          </div>
+  return (
+    <section>
+      <div className="divider">Datos Personales</div>
+      <div className="field">
+        <label className="label">Nombre</label>
+        <div className="control">
+          <input
+            name="nombre"
+            className="input shadow-lg"
+            value={persona.nombre}
+            onChange={e => setPersona({ ...persona, [e.target.name]: e.target.value })}
+            type="text"
+          />
         </div>
-        <div className="field">
-          <label className="label">Email</label>
-          <div className="control">
-            <input className="input shadow-lg" type="email" />
-          </div>
+      </div>
+      <div className="field">
+        <label className="label">Email</label>
+        <div className="control">
+          <input
+            name="email"
+            className="input shadow-lg"
+            value={persona.email}
+            onChange={e => setPersona({ ...persona, [e.target.name]: e.target.value })}
+            type="email"
+          />
         </div>
-        <div className="columns">
-          <div className="column">
-            <div className="field">
-              <label className="label">Número de Documento</label>
-              <div className="control">
-                <input
-                  className="input shadow-lg"
-                  type="text"
-                />
-              </div>
+      </div>
+      <div className="columns">
+        <div className="column">
+          <div className="field">
+            <label className="label">Número de Documento</label>
+            <div className="control">
+              <input
+                name="nro_documento"
+                className="input shadow-lg"
+                value={persona.nro_documento}
+                onChange={e => setPersona({ ...persona, [e.target.name]: e.target.value })}
+                type="text"
+              />
             </div>
           </div>
-          <div className="column">
-            <div className="field">
-              <label className="label">Tipo de Documento</label>
-              <div className="control">
-                <Select 
-                  className="shadow-lg"
-                  placeholder=""
-                  options={options} 
-                />
-              </div>
+        </div>
+        <div className="column">
+          <div className="field">
+            <label className="label">Tipo de Documento</label>
+            <div className="control">
+              <Select
+                className="shadow-lg"
+                placeholder=""
+                onChange={e => setPersona({ ...persona, tip_documento: e.value })}
+                options={options}
+              />
             </div>
           </div>
         </div>
-      </section>
-    )
+      </div>
+    </section>
+  )
+};
+
+const CrearUsuario = () => {
+  const empleado = { 
+                    empleado: {},
+                    usuario: {},
+                    nombre: "",
+                    email: "",
+                    nro_documento: "",
+                    tip_documento: ""
+                  };
+  const [persona, setPersona] = useState({...empleado});
+  const [modalIsOpen, setIsOpen] = useState(false);
+console.log(persona)
+  const crear = async e => {
+    e.preventDefault();
+    await createUsuarios(persona);
   };
 
   return (
@@ -77,7 +93,7 @@ const CrearUsuario = () => {
       <section className="section sm:w-1/2 w-full m-auto hero shadow-lg shadow-gray-800 bg-white">
         <h1 className="title is-3 text-center">Nuevo Empleado</h1>
         <form>
-          <DatosPersona />
+          <DatosPersona persona={persona} setPersona={setPersona} />
           <ModalUsuario modalIsOpen={modalIsOpen} setIsOpen={setIsOpen} />
           <div className="field mt-3">
             <div className="control">
