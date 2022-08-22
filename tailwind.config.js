a6917da1eb983609029732cb24ab0069b963a9f5
Nelba Barreto <barretonelba@gmail.com>
2022-08-22 00:23:52 -0400

    Agregar menús wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,19 @@ module.exports = {
     extend: {
       colors: {
         lila: {
+          200: "#F0F3F9",
+          300: "#6c6594",
+          350: "#3F3A59",
           400: "#604fcd",
           700: "#1E40AF",
           500: "#130b43",
         }
+      },
+      gridTemplateColumns: {
+        layout: "18rem 1fr",
+      },
+      gridTemplateRows: {
+        layout: "80px 1fr 80px",
       },
     },
   },
