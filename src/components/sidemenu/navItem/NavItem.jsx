f2a5e34444f8d8ce6084df8da09d60f5b87aeb0c
Nelba Barreto <barretonelba@gmail.com>
2022-08-23 05:43:20 -0400

    Avances en el estilo del navItemHeader

--- src/components/sidemenu/navItem/NavItem.jsx
+++ src/components/sidemenu/navItem/NavItem.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import style from "./navItem.module.css";
 import NavItemHeader from "./NavItemHeader.jsx";
 
 const NavItem = ({ item }) => {
--- src/components/sidemenu/navItem/NavItemHeader.jsx
+++ src/components/sidemenu/navItem/NavItemHeader.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import style from "./navItem.module.css";
 
 const resolveLinkPath = (childTo, parentTo) => `${parentTo}/${childTo}`;
@@ -17,20 +19,25 @@ const NavItemHeader = props => {
     e.preventDefault();
     setExpand(expanded => !expanded);
   };
-	
+
   return (
     <>
       <button
-        className={`${style.navItem} ${style.navItemHeaderButton}`}
+        //className={`${style.navItem} ${style.navItemHeaderButton}`}
+        className="flex items-center no-underline py-3 px-5 text-lila-500 hover:bg-lila-200
+                   hover:text-lila-350 w-full border-none bg-transparent cursor-pointer outline-none"
         onClick={onExpandChange}
       >
         <Icon /*className={style.navIcon}*/ />
-        <span className={style.navLabel}>{label}</span>
+        <span className="text-lila-500 hover:text-lila-350 text-base">{label}</span>
         {/* <ChevronDownIcon
           className={`${style.navItemHeaderChevron} ${
             expanded && style.chevronExpanded
           }`}
         /> */}
+        <span className="icon">
+          <FontAwesomeIcon icon={solid("chevron-down")} />
+        </span>
       </button>
 
       {expanded && (
