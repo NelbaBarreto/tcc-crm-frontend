c19adb9adab514ffc5d95433ee15580a6227cd19
Nelba Barreto <barretonelba@gmail.com>
2022-09-25 16:22:48 -0400

    Volver a agregar carpeta de educación

---
+++ src/components/educacion/profesores/CrearProfesor.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+
+const CrearProfesor = () => {
+  return (
+    <div>
+      <section className="section w-full m-auto">
+        <h1 className="title is-3 text-center">Nuevo Profesor</h1>
+        <form>
+
+          <div className="columns is-desktop">
+            <div className="column">
+              <div className="field">
+                <label className="label">Nombre Completo</label>
+                <div className="control">
+                  <input
+                    name="nombre"
+                    className="input shadow-lg"
+                    type="text"
+                    placeholder="Ingrese el nombre del profesor"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="field">
+                <label className="label">Fecha de Registro</label>
+                <div className="control">
+                  <input
+                    name="fecIni"
+                    className="input shadow-lg"
+                    type="date"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="columns is-mobile">
+            <div className="column">
+              <div className="field">
+                <label className="label">Tipo Documento</label>
+                <div className="control">
+                  <div className="select">
+                    <select>
+                      <option>Cedula de Indetidad Paraguaya</option>
+                      <option>Registro Unico de Contribuyente</option>
+                      <option>Tarjeta Diplomatica</option>
+                      <option>Cedula de Indentidad Extranjera</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="field">
+                <label className="label">Numero de Documento</label>
+                <div className="control">
+                  <input
+                    name="codCaso"
+                    className="input shadow-lg"
+                    type="number"
+                    placeholder="Ingrese su numero de documento"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div className="columns is-mobile">
+            <div className="column">
+              <div className="field">
+                <label className="label">Correo</label>
+                <div className="control">
+                  <input
+                    name="nombre"
+                    className="input shadow-lg"
+                    type="email"
+                    placeholder="Ingrese su mail"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="field">
+                <label className="label">Codigo Curso</label>
+                <div className="control">
+                  <input
+                    name="curso_id"
+                    className="input shadow-lg"
+                    type="number"
+                    placeholder="Ingrese el Codigo del curso"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="columns is-desktop">
+            <div className="column">
+              <div className="field">
+                <label className="label">Tipo Telefono</label>
+                <div className="control">
+                  <div className="select">
+                    <select>
+                      <option>Movil</option>
+                      <option>Familiar</option>
+                      <option>Laboral</option>
+                      <option>Otro</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="field">
+                <label className="label">Numero de Telefono</label>
+                <div className="control">
+                  <input
+                    name="numero"
+                    className="input shadow-lg"
+                    type="number"
+                    placeholder="Ingrese el numero de telefono"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="field mt-3">
+            <div className="control">
+              <button
+                className="button float-right font-semibold shadow-lg text-white hover:text-white focus:text-white
+                 hover:bg-deep-purple-700 bg-deep-purple-400 border-deep-purple-700"
+              >
+                Guardar
+              </button>
+            </div>
+          </div>
+        </form>
+      </section>
+    </div>
+  )
+};
+
+export default CrearProfesor;
---
+++ src/components/educacion/sucursales/CrearSucursal.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+const CrearSucursal = () => {
+  return (
+    <div>
+      <section className="section w-full m-auto">
+        <h1 className="title is-3 text-center">Nueva Sucursal</h1>
+        <form>
+
+          <div className="columns is-desktop">
+            <div className="column">
+              <div className="field">
+                <label className="label">Nombre</label>
+                <div className="control">
+                  <input
+                    name="nombre"
+                    className="input shadow-lg"
+                    type="text"
+                    placeholder="Ingrese el nombre de la sucursal"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+            <div className="field">
+                <label className="label">Dirección</label>
+                <div className="control">
+                  <input
+                    name="direccion_id"
+                    className="input shadow-lg"
+                    type="text"
+                    placeholder="Ingrese la direccion de la sucursal"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="columns is-desktop">
+            <div className="column">
+            <div className="field">
+                <label className="label">Tipo Telefono</label>
+                <div className="control">
+                  <div className="select">
+                    <select>
+                      <option>Linea fija</option>
+                      <option>Movil</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="field">
+                <label className="label">Numero de Telefono</label>
+                <div className="control">
+                  <input
+                    name="numero"
+                    className="input shadow-lg"
+                    type="number"
+                    placeholder="Ingrese el numero de telefono"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="field mt-3">
+            <div className="control">
+              <button
+                className="button float-right font-semibold shadow-lg text-white hover:text-white focus:text-white
+                 hover:bg-deep-purple-700 bg-deep-purple-400 border-deep-purple-700"
+              >
+                Guardar
+              </button>
+            </div>
+          </div>
+        </form>
+      </section>
+    </div>
+  )
+};
+
+export default CrearSucursal;
