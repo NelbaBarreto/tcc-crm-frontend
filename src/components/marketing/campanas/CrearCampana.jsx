1f4ccf2620216908a1e5bde32ae463d1152ecfe1
Nelba Barreto <barretonelba@gmail.com>
2022-09-25 22:10:36 -0400

    Redireccionar al crear nueva campaña

--- src/components/marketing/campanas/CrearCampana.jsx
+++ src/components/marketing/campanas/CrearCampana.jsx
@@ -1,72 +1,74 @@
 import React, { useState } from "react";
+import MostrarMensaje from "../../formulario/MostrarMensaje";
+import Seccion from "../../formulario/Seccion";
 import { Volver, Guardar } from "../../formulario/Acciones";
 import { useNavigate } from "react-router-dom";
 import { createCampana } from "../../../api/campanas";
+import { Titulo1 } from "../../formulario/Titulo";
 
 const CrearCampana = () => {
   const [campana, setCampana] = useState({});
+  const [state, setState] = useState(false);
   const navigate = useNavigate();
 
   const crear = async e => {
     e.preventDefault();
-    await createCampana(campana);
+    setState({ saving: true, error: false, message: "" });
+    try {
+      await createCampana(campana);
+      setState({ saving: false, error: false, message: "Campaña creada exitosamente." });
+      setTimeout(() => navigate("/marketing/campanas"), 3000);
+    } catch (e) {
+      setState({ saving: false, error: true, message: e.message });
+    };
   };
 
-  console.log(campana);
 
   return (
     <div>
       <section className="section w-full m-auto">
-        <h1 className="title is-3 text-center">Nueva Campaña</h1>
+        <Titulo1>
+          Nueva Campaña
+        </Titulo1>
+        {state.message ? <MostrarMensaje mensaje={state.message} error={state.error} /> : null}
         <form>
-          <div className="field">
-            <label className="label">Nombre de la Campaña</label>
-            <div className="control">
-              <input
-                name="nombre"
-                className="input shadow-lg"
-                type="text"
-                onChange={e => setCampana({ ...campana, [e.target.name]: e.target.value })}
-              />
+          <Seccion titulo="Datos de la Campaña">
+            <div className="field">
+              <label className="label">Nombre</label>
+              <div className="control">
+                <input
+                  name="nombre"
+                  className="input shadow-lg"
+                  type="text"
+                  onChange={e => setCampana({ ...campana, [e.target.name]: e.target.value })}
+                />
+              </div>
             </div>
-          </div>
+            <div className="field">
+              <label className="label">Fecha de Inicio</label>
+              <div className="control">
+                <input
+                  name="fec_inicio"
+                  className="input shadow-lg"
+                  type="date"
+                  onChange={e => setCampana({ ...campana, [e.target.name]: e.target.value })}
+                />
+              </div>
+            </div>
 
-          <div className="field">
-            <label className="label">Tipo Campaña</label>
-            <div className="control">
-              <input
-                name="tip_campana_id"
-                className="input shadow-lg"
-                type="number"
-                onChange={e => setCampana({ ...campana, [e.target.name]: e.target.value })}
-              />
+            <div className="field">
+              <label className="label">Fecha de Fin</label>
+              <div className="control">
+                <input
+                  name="fec_fin"
+                  className="input shadow-lg"
+                  type="date"
+                  onChange={e => setCampana({ ...campana, [e.target.name]: e.target.value })}
+                />
+              </div>
             </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Fecha Inicio</label>
-            <div className="control">
-              <input
-                name="fec_inicio"
-                className="input shadow-lg"
-                type="date"
-                onChange={e => setCampana({ ...campana, [e.target.name]: e.target.value })}
-              />
-            </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Fecha Fin</label>
-            <div className="control">
-              <input
-                name="fec_fin"
-                className="input shadow-lg"
-                type="date"
-                onChange={e => setCampana({ ...campana, [e.target.name]: e.target.value })}
-              />
-            </div>
-          </div>
-          <Guardar guardar={crear} />
+          </Seccion>
+          <Guardar guardar={crear} saving={state.saving} />
         </form>
         <Volver navigate={navigate} />
       </section>
