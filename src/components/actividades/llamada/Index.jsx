5338dd66b3d7e25cc5f6d6b7acb10bad018f81da
david-rkto <rkto737@gmail.com>
2022-10-18 14:29:30 -0300

    Index Llamada

---
+++ src/components/actividades/llamada/Index.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import DataTables from "../../DataTables";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useQuery } from "react-query";
+import { getLlamadas } from "../../../api/llamadas";
+import { NavLink } from "react-router-dom";
+
+const Index = () => {
+  const {
+    data: llamadas,
+    isLoading
+  } = useQuery(["llamadas"], getLlamadas);
+
+  const columns = [
+    {
+      name: "llamada_id",
+      options: {
+        display: "excluded",
+        filter: false
+      }
+    },
+    {
+      name: "asunto",
+      label: "Asunto",
+      options: {
+        filter: true,
+        filterType: "textField",
+        sort: true,
+      }
+    },
+    {
+      name: "tipo",
+      label: "Tipo",
+      options: {
+        filter: true,
+        filterType: "textField",
+        sort: true,
+      }
+    },
+    {
+      name: "estado",
+      label: "Estado",
+      options: {
+        filter: true,
+        filterType: "textField",
+        sort: true,
+      }
+    },
+    {
+      name: "",
+      options: {
+        filter: false,
+        sort: false,
+        empty: true,
+        customBodyRenderLite: (dataIndex, _rowIndex) => {
+          return (
+            <button
+              className="button font-semibold shadow-lg text-white hover:text-white focus:text-white
+              hover:bg-deep-purple-700 bg-deep-purple-400 border-deep-purple-700"
+              onClick={() => console.log(llamadas[dataIndex])}
+            >
+              Editar
+            </button>
+          );
+        }
+      }
+    },
+  ];
+
+  return (
+    <div>
+      <section className="section w-full m-auto">
+        <NavLink
+          to="/actividades/llamadas/nuevo"
+          className="button font-semibold shadow-lg text-white hover:text-white focus:text-white
+              hover:bg-deep-purple-700 bg-deep-purple-400 border-deep-purple-700 mb-2"
+        >
+          <span>Crear Nuevo</span>
+          <span className="icon is-small">
+            <FontAwesomeIcon icon={solid("plus")} />
+          </span>
+        </NavLink>
+        <DataTables
+          title="Listado de Llamadas"
+          columns={columns}
+          data={llamadas}
+          isLoading={isLoading}
+        />
+      </section>
+    </div>
+  )
+}
+
+export default Index;
