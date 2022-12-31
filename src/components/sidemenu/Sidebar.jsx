ee1a53194f5258537aff35ecd09715f9b001bcff
Nelba Barreto <barretonelba@gmail.com>
2022-12-31 16:59:03 -0300

    Agregar overflow a sidebar si supera el largo de la pantalla

--- src/components/sidemenu/Sidebar.jsx
+++ src/components/sidemenu/Sidebar.jsx
@@ -5,7 +5,7 @@ import { sideMenu } from "./menu.config.js";
 const Sidebar = () => {
   return (
     <div
-      className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-hidden text-center bg-white shadow-md"
+      className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-white shadow-md"
     >
       <div className="text-gray-100 text-xl">
         <div className="p-2.5 mt-1 flex items-center">
