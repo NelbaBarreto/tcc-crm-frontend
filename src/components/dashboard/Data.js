1e6ba85c49c7d8016028d07301c05f0248ccdca9
david-rkto <rkto737@gmail.com>
2023-01-08 16:54:52 -0300

    Datos para grafico ChartJS

---
+++ src/components/dashboard/Data.js
@@ -0,0 +1,38 @@
+export const UserData = [
+    {
+        id: 1,
+        anho: 2016,
+        LeadGanados: 80000,
+        LeadPerdidos: 823,
+    },
+    {
+        id: 2,
+        anho: 2017,
+        LeadGanados: 6000,
+        LeadPerdidos: 725,
+    },
+    {
+        id: 3,
+        anho: 2018,
+        LeadGanados: 18600,
+        LeadPerdidos: 276,
+    },
+    {
+        id: 4,
+        anho: 2019,
+        LeadGanados: 95000,
+        LeadPerdidos: 892,
+    },
+    {
+        id: 5,
+        anho: 2020,
+        LeadGanados: 15000,
+        LeadPerdidos: 127,
+    },
+    {
+        id: 6,
+        anho: 2021,
+        LeadGanados: 45700,
+        LeadPerdidos: 350,
+    },
+]
