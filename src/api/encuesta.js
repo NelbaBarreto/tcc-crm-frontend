8b2ff8e7bba67e32a4a762c690eaafbba1f1687a
Nelba Barreto <barretonelba@gmail.com>
2023-01-15 22:26:32 -0300

    Guardar respuestas a encuesta en la bd

--- src/api/encuesta.js
+++ src/api/encuesta.js
@@ -6,3 +6,14 @@ export const getPreguntas = async () => {
   const { data: response } = await axios.get(`${API}/encuesta/preguntas`);
   return response.data;
 };
+
+export const validarToken = async (data) => {
+  const { data: response } = await axios.post(`${API}/encuesta/validarToken`, data);
+  return response.data;
+};
+
+export const enviarEncuesta = async (data) => {
+  console.log(data)
+  const { data: response } = await axios.post(`${API}/encuesta/enviar`, data);
+  return response.data;
+};
--- src/components/formulario/Acciones.jsx
+++ src/components/formulario/Acciones.jsx
@@ -1,14 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import classNames from "classnames";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import {} from "@fortawesome/free-solid-svg-icons";
-
-
 import { classNameButton2 } from "./Componentes";
 
-export const Guardar = ({ guardar, saving = false }) => {
+export const Guardar = ({ guardar, saving = false, label = "Guardar", disabled }) => {
   return (
     <div className="field mt-3">
       <div className="control">
@@ -16,10 +12,10 @@ export const Guardar = ({ guardar, saving = false }) => {
           type="submit"
           className={classNames(`${classNameButton2} float-right`,
             { "is-loading": saving })}
-          disabled={saving}
+          disabled={saving || disabled}
           onClick={e => guardar(e)}
         >
-          <span>Guardar</span>
+          <span>{label}</span>
           <span className="icon is-small">
             <FontAwesomeIcon icon={solid("floppy-disk")} />
           </span>
@@ -47,8 +43,6 @@ export const Eliminar = ({ eliminar }) => {
   return (
     <button
       className={`${classNameButton2} float-right`}
-      // "button font-semibold shadow-lg text-deep-purple-800 hover:text-white focus:text-white
-      // hover:bg-deep-purple-700 bg-deep-purple-100"
       onClick={e => eliminar(e)}
     >
       <span>Eliminar</span>
