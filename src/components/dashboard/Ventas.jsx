b83393cd6bc4093202ed1cc00cce96849a6b6df3
Nelba Barreto <barretonelba@gmail.com>
2023-01-27 19:58:23 -0300

    Agregar gráficos para ventas

--- src/components/dashboard/Ventas.jsx
+++ src/components/dashboard/Ventas.jsx
@@ -3,69 +3,75 @@ import BarChart from "./assets/js/BarChart";
 import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import {
-  getCasosPorEstado,
+  getLeadsPorEstado,
   getLeadsPorOrigen
 } from "../../api/dashboard";
 
 const MainDash = () => {
-  const [estadoData, setEstadoData] = useState({ datasets: [] });
+  const [leadEstadoData, setLeadEstadoData] = useState({ datasets: [] });
   const [leadOrigenData, setLeadOrigenData] = useState({ datasets: [] });
 
   const {
-    data: casosPorEstado,
-    casosEFetching
-  } = useQuery(["casosPorEstado"], getCasosPorEstado);
+    data: leadsPorEstado,
+    leadsEFetching
+  } = useQuery(["leadsPorEstado"], getLeadsPorEstado);
 
   const {
     data: leadsPorOrigen,
     leadsOFetching
   } = useQuery(["leadsPorOrigen"], getLeadsPorOrigen);
 
-  console.log(leadsPorOrigen)
 
   useEffect(() => {
-    if (!casosEFetching) {
-      setEstadoData({
-        labels: casosPorEstado?.map((data) => data.estado),
+    if (!leadsEFetching) {
+      setLeadEstadoData({
+        labels: leadsPorEstado?.map((data) => data.estado),
         datasets: [{
-          label: "Casos",
-          data: casosPorEstado?.map((data) => data.total),
+          label: "Leads",
+          data: leadsPorEstado?.map((data) => data.total),
           backgroundColor: ["#3296ed", "#96d44e", "#c398f5", "#50d4cd"]
         },],
       });
     }
-  }, [casosPorEstado, casosEFetching]);
+  }, [leadsPorEstado, leadsEFetching]);
 
   useEffect(() => {
-    if (!leadsOFetching && leadsPorOrigen.length) {
+    if (!leadsOFetching && leadsPorOrigen?.length) {
       setLeadOrigenData(
         {
           labels: leadsPorOrigen[0]?.map((data) => data.origen),
-          datasets: [{
-            label: "Leads",
-            data: leadsPorOrigen[0]?.map((data) => data.total),
-            backgroundColor: ["#3296ed", "#96d44e", "#c398f5", "#50d4cd"]
-          },],
-        },
-        {
-          labels: leadsPorOrigen[1]?.map((data) => data.origen),
-          datasets: [{
-            label: "Leads Convertidos",
-            data: leadsPorOrigen[1]?.map((data) => data.total),
-            backgroundColor: ["#3e51b5", "#96d44e", "#c398f5", "#50d4cd"]
-          },],
-        });
+          datasets: [
+            {
+              label: "Leads",
+              data: leadsPorOrigen[0]?.map((data) => data.total),
+              backgroundColor: ["#00bfc4"]
+            },
+            {
+              label: "Leads Convertidos",
+              data: leadsPorOrigen[1]?.map((data) => data.total),
+              backgroundColor: ["#c77cff"]
+            }
+          ],
+        }
+      )
     }
   }, [leadsPorOrigen, leadsOFetching]);
 
   return (
     <div
-      className="grid sm:grid-rows-3 sm:grid-cols-1 md:grid-cols-3 md:grid-rows-1 gap-1"
+      className="grid grid-cols-2 gap-1"
     >
       <div className="rounded-md shadow-md bg-white p-2">
         <BarChart
           chartData={leadOrigenData}
+          stacked={true}
           title="Leads y Leads Convertidos Por Origen"
+        />
+      </div>
+      <div className="rounded-md shadow-md bg-white p-2">
+        <BarChart
+          chartData={leadEstadoData}
+          title="Leads Por Estado"
         />
       </div>
     </div>
--- src/components/dashboard/assets/js/BarChart.jsx
+++ src/components/dashboard/assets/js/BarChart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto"
 
-const BarChart = ({ chartData, title }) => {
+const BarChart = ({ chartData, title, stacked = false }) => {
   return (
     <Bar
       data={chartData}
@@ -15,7 +15,19 @@ const BarChart = ({ chartData, title }) => {
             display: true,
             text: title
           }
-        }
+        },
+        responsive: true,
+        scales: {
+          x: {
+            stacked: stacked,
+          },
+          y: {
+            stacked: stacked,
+            ticks: {
+              precision: 0,
+            },
+          }
+        },
       }} />
   );
 }
