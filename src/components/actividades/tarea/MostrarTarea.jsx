099a8df343ac340965a40320a92e8d588a13589f
Nelba Barreto <barretonelba@gmail.com>
2023-01-30 00:08:21 -0300

    Corregir hora de mostrar tarea

--- src/components/actividades/tarea/MostrarTarea.jsx
+++ src/components/actividades/tarea/MostrarTarea.jsx
@@ -6,22 +6,9 @@ import { Volver } from "../../formulario/Acciones";
 import { useQuery } from "react-query";
 import { useParams, useNavigate } from "react-router-dom";
 import { getTarea } from "../../../api/tareas";
-import { getLead } from "../../../api/leads";
-import { getContacto } from "../../../api/contactos";
 import { format } from "date-fns";
 
 const DatosTarea = ({ tarea = {} }) => {
-
-  const {
-    data: lead,
-  } = useQuery(["lead", tarea.lead_id], () => getLead(tarea.lead_id));
-
-  const {
-    data: contacto,
-  } = useQuery(["contacto", tarea.contacto_id], () => getContacto(tarea.contacto_id));
-
-  console.log(contacto);
-
   return (
     <Seccion titulo={tarea.asunto}>
       <div className="columns">
@@ -34,10 +21,10 @@ const DatosTarea = ({ tarea = {} }) => {
       </div>
       <div className="columns">
         <div className="column">
-          <TextView label="Fecha de Inicio" value={format(tarea.fec_inicio, "dd/MM/yyyy hh:mm")} />
+          <TextView label="Fecha de Inicio" value={tarea.fec_inicio ? format(tarea.fec_inicio, "dd/MM/yyyy") : ""} />
         </div>
         <div className="column">
-          <TextView label="Fecha Fin" value={format(tarea.fec_fin, "dd/MM/yyyy hh:mm")} />
+          <TextView label="Fecha Fin" value={tarea.fec_fin ? format(tarea.fec_fin, "dd/MM/yyyy") : ""} />
         </div>
       </div>
       <div className="columns">
@@ -55,10 +42,10 @@ const DatosTarea = ({ tarea = {} }) => {
       </div>
       <div className="columns">
         <div className="column">
-          <TextView label="Contacto" value={`${contacto.contacto_id}-${contacto.persona.nombre}`} />
+          <TextView label="Lead" value={tarea.lead?.persona.nombre} />
         </div>
         <div className="column">
-          <TextView label="Lead" value={`${lead.lead_id}-${lead.persona.nombre}`} />
+          <TextView label="Contacto" value={tarea.contacto?.persona.nombre} />
         </div>
       </div>
     </Seccion >
