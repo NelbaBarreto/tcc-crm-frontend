1a9f99550c68f9c273fde91589d886a720dac7ec
n-barreto-excelsis <nbarreto@excelsis.com.py>
2023-02-01 00:13:55 -0300

    Ciclos wip

--- src/components/educacion/ciclos/CrearCiclo.jsx
+++ src/components/educacion/ciclos/CrearCiclo.jsx
@@ -1,82 +1,76 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AppContext from "../../../utils/AppContext";
+import useToken from "../../../utils/useToken";
 import Seccion from "../../formulario/Seccion";
 import MostrarMensaje from "../../formulario/MostrarMensaje";
-import { Titulo1 } from "../../formulario/Titulo";
-import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Dropdown, Datepicker, Input } from "../../formulario/Componentes";
 import { Volver, Guardar } from "../../formulario/Acciones";
-import { useNavigate } from "react-router-dom";
-import { useQuery } from "react-query";
+import { Titulo1 } from "../../formulario/Titulo";
+import { Dropdown, Datepicker } from "../../formulario/Componentes";
 import { getSedes } from "../../../api/sedes";
-import { CheckboxGroup } from "../../formulario/Componentes";
-import AppContext from "../../../utils/AppContext";
+import { getCursos } from "../../../api/cursos";
+import { createCiclo } from "../../../api/ciclos";
+import { handleDispatch, handleStateCleared } from "../../formulario/reducerFormularios.js";
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 
-const DatosCiclo = ({ onChange, curso }) => {
-  const [select, setSelect] = useState({ nivel: "" });
+const CICLO = "ciclo";
+
+const DatosCiclo = ({ ciclo = {}, dispatch, select = {} }) => {
+  const {
+    data: cursos,
+    cursosLoading
+  } = useQuery(["cursos"], getCursos);
 
   const {
     data: sedes,
     sedesLoading
   } = useQuery(["sedes"], getSedes);
 
-  const opcionesSede = sedesLoading || !sedes ? [] :
+  const opcionesSedes = sedesLoading || !sedes ? [] :
     sedes.map(sede => ({ value: sede.sede_id, label: sede.nombre }));
+
+  const opcionesCursos = cursosLoading || !cursos ? [] :
+    cursos.map(curso => ({ value: curso.curso_id, label: curso.nombre }));
 
   return (
     <Seccion titulo="Datos del Ciclo">
       <div className="columns">
         <div className="column">
           <Dropdown
-            label="Nivel"
-            onChange={e => { onChange(e, "nivel", e?.value); setSelect({ ...select, nivel: e }) }}
-            value={select.nivel}
-            options={[{ label: "Avanzado", value: "avanzado" }]}
+            label="Curso*"
+            options={opcionesCursos}
+            value={select.curso}
+            onChange={e => {
+              handleDispatch(dispatch, "curso_id", e?.value, CICLO);
+              handleDispatch(dispatch, "curso", e, "select")
+            }}
           />
         </div>
         <div className="column">
           <Dropdown
-            label="Sede"
-            onChange={e => { onChange(e, "sede_id", e?.value); setSelect({ ...select, sede: e }) }}
+            label="Sede*"
+            options={opcionesSedes}
             value={select.sede}
-            options={opcionesSede}
-          />
-        </div>
-      </div>
-      <div className="columns">
-        <div className="column">
-          <Input
-            name="precio"
-            label="Precio"
-            value={curso?.precio}
-            onChange={onChange}
-          />
-        </div>
-        <div className="column">
-          <Dropdown
-            label="Modalidad"
-            onChange={e => { onChange(e, "modalidad", e?.value); setSelect({ ...select, modalidad: e }) }}
-            value={select.modalidad}
-            options={[{ label: "Presencial", value: "presencial" }]}
-          //options={opcionesSede}
+            onChange={e => {
+              handleDispatch(dispatch, "sede_id", e?.value, CICLO);
+              handleDispatch(dispatch, "sede", e, "select")
+            }}
           />
         </div>
       </div>
       <div className="columns">
         <div className="column">
           <Datepicker
-            label="Fecha de Inicio"
-            name="fec_ini_curso"
-            selected={curso?.fec_ini_curso}
-            onChange={date => onChange(null, "fec_ini_curso", date)}
+            label="Fecha de Inicio*"
+            selected={ciclo?.fec_inicio || ""}
+            onChange={fecha => handleDispatch(dispatch, "fec_inicio", fecha, CICLO)}
           />
         </div>
         <div className="column">
           <Datepicker
-            label="Fecha Fin"
-            name="fec_fin_curso"
-            selected={curso?.fec_fin_curso}
-            onChange={date => onChange(null, "fec_fin_curso", date)}
+            label="Fecha Fin*"
+            selected={ciclo?.fec_fin || ""}
+            onChange={fecha => handleDispatch(dispatch, "fec_fin", fecha, CICLO)}
           />
         </div>
       </div>
@@ -84,111 +78,26 @@ const DatosCiclo = ({ onChange, curso }) => {
   );
 };
 
-const MostrarAulas = () => {
-  return (
-    <table className="table is-striped is-narrow is-fullwidth">
-      <thead>
-        <tr>
-          <th>Aula</th>
-          <th>Cant. Máxima Alumnos</th>
-          <th>Horario</th>
-          <th>Días</th>
-          <th></th>
-        </tr>
-      </thead>
-    </table>
-  );
-}
-
-const DatosAulas = ({ aula }) => {
-  const [select, setSelect] = useState({ dias: [] });
-  const opcionesDias = [
-    { label: "Lunes", value: "lunes" },
-    { label: "Martes", value: "martes" },
-    { label: "Miércoles", value: "miercoles" },
-    { label: "Jueves", value: "jueves" },
-    { label: "Viernes", value: "viernes" },
-    { label: "Sábado", value: "sabado" },
-    { label: "Domingo", value: "domingo" }
-  ];
-
-  return (
-    <Seccion titulo="Agregar Aulas">
-      <div className="columns">
-        <div className="column is-three-fifths">
-          <div className="columns">
-            <div className="column">
-              <Dropdown
-                label="Aula"
-                //onChange={e => { onChange(e, "nivel", e.value); setNivel(e) }}
-                //value={nivel}
-                options={[{ label: "Avanzado", value: "avanzado" }]}
-              />
-              <Input
-                name="hor_inicio"
-                label="Hora Inicio"
-                //value={curso?.precio}
-                onChange={onChange}
-              />
-              <CheckboxGroup 
-                label={"Días de clases"} 
-                options={opcionesDias}
-                name="dias"
-                value={"Lunes"}
-                onChange={e => console.log(e)}
-              />
-            </div>
-            <div className="column">
-              <Input
-                name="cantidad_alumnos"
-                label="Cantidad Máxima de Alumnos"
-                //value={curso?.precio}
-                onChange={onChange}
-              />
-              <Input
-                name="hor_fin"
-                label="Hora Fin"
-                //value={curso?.precio}
-                onChange={onChange}
-              />
-              <div className="field mt-3">
-                <div className="control">
-                  <button
-                    onClick={console.log("a")}
-                  >
-                    <span>Agregar</span>
-                    <span className="icon is-small">
-                      <FontAwesomeIcon icon={solid("plus")} />
-                    </span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="column">
-          <MostrarAulas />
-        </div>
-      </div>
-    </Seccion>
-  );
-}
-
 const CrearCiclo = () => {
-  const { state: { aula }, dispatch } = useContext(AppContext);
+  const { state: { ciclo, select }, dispatch } = useContext(AppContext);
   const [action, setAction] = useState({});
+  const { usuario = {} } = useToken();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    handleStateCleared(dispatch);
+  }, []);
 
   const crear = async e => {
     e.preventDefault();
-    // setAction({ saving: true, error: false, message: "" });
-    // try {
-    //   await createCurso(state.curso);
-    //   setAction({ saving: false, error: false, message: "Curso creado exitosamente." });
-    //   setTimeout(() => navigate("/educacion/cursos"), 3000);
-    // } catch (e) {
-    //   setAction({ saving: false, error: true, message: e.message });
-    // };
+    setAction({ saving: true, error: false, message: "" });
+    try {
+      await createCiclo({ ...ciclo, usu_insercion: usuario.nom_usuario, usu_modificacion: usuario.nom_usuario });
+      setAction({ saving: false, error: false, message: "Ciclo creado exitosamente." });
+      setTimeout(() => navigate("/educacion/ciclos"), 2000);
+    } catch (e) {
+      setAction({ saving: false, error: true, message: e.message });
+    };
   };
 
   return (
@@ -199,17 +108,18 @@ const CrearCiclo = () => {
         </Titulo1>
         {action.message ? <MostrarMensaje mensaje={action.message} error={action.error} /> : null}
         <form>
-          <DatosCiclo 
-            onChange={handleDispatch} 
-            curso={state.curso} 
-
+          <DatosCiclo
+            ciclo={ciclo}
+            select={select}
+            dispatch={dispatch}
           />
-          <DatosAulas 
-            // onChange={handleDispatch} 
-            aula={aula} 
+          <Guardar
+            saving={action.saving}
+            guardar={crear}
           />
-          <Guardar saving={action.saving} guardar={crear} />
-          <Volver navigate={navigate} />
+          <Volver
+            navigate={navigate}
+          />
         </form>
       </section>
     </div>
